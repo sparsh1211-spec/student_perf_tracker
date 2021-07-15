5fd68c32587793da881c7b551701caeab7647743
sparsh1211-spec <84899495+sparsh1211-spec@users.noreply.github.com>
2021-07-15 17:00:00 +0530

    added test file

---
+++ src/pages/Testpage.tsx
@@ -0,0 +1,19 @@
+
+import React from "react";
+
+interface Props{
+   
+}
+
+const Testpage: React.FC<Props>=(props) => {
+    return (
+        <>
+            
+        </>
+    );
+
+};
+Testpage.defaultProps={
+}
+
+export default React.memo(Testpage);
