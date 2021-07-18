898719c9ea1a65232859737a52eb89500a0d6440
sparsh1211-spec <84899495+sparsh1211-spec@users.noreply.github.com>
2021-07-18 16:31:43 +0530

    added UI in login page

--- src/pages/Login.page.tsx
+++ src/pages/Login.page.tsx
@@ -158,13 +158,13 @@ const Login: React.FC<Props> = (props) => {
 
 <>
            
-            <form className="m-auto " onSubmit={handleSubmit}>
-                 <div className="mb-6 text-2xl font-semibold text-blue-600">Log In to <span className="font-extrabold">DEVS</span></div>
+            <form className="mt-32 ml-36" onSubmit={handleSubmit}>
+                 <div className="mb-6 text-5xl font-semibold text-blue-600">Log In to <span className="font-extrabold">DEVS</span></div>
                 
                 <div className="mb-12 font-bold">New here? <Link to="/auth/signup" className="text-indigo-600 underline hover:text-indigo-900">Create an account</Link></div>
                 <div className="flex flex-col space-y-10">
 
-                    <div className="flex items-center justify-center space-x-4 text-left border-b border-gray-300">
+                    <div className="flex items-center space-x-4 text-left border-b border-gray-300">
                         <label htmlFor="email" className="sr-only" />
                         <FaUserCircle className="text-indigo-600" style={{ margin: 0 + 'px', }} />
                         <input className="outline-none "
@@ -181,7 +181,7 @@ const Login: React.FC<Props> = (props) => {
                     {touched.email && <div className="text-red-500 ">{errors.email}</div>}
 
 
-                    <div className="flex items-center justify-center space-x-4 text-left border-b border-gray-300">
+                    <div className="flex items-center space-x-4 text-left border-b border-gray-300">
                         <label htmlFor="Password:" />
                         <RiLockPasswordLine className="text-indigo-600" style={{ margin: 0 + 'px' }} />
                         <input className="outline-none"
@@ -195,22 +195,8 @@ const Login: React.FC<Props> = (props) => {
 
                 </div>
                 {isSubmitting && <FaSpinner className="mx-auto mt-8 animate-spin" />}
-                <div className="flex flex-row space-x-4 mt-11">
-
-                    <button type="submit" className="p-1 px-4 text-white bg-indigo-500 border-2 rounded-md shadow-md hover:opacity-60">Login</button>
-
-
-                    <button type="button" onClick={(event) => {
-                        console.log("cancel button clicked");
-                        event.preventDefault();
-
-                    }} className="p-1 px-4 text-white bg-indigo-500 border-2 rounded-md shadow-md hover:opacity-60">Cancel</button>
-
-
-                </div>
-                <div className="flex items-center mt-4 space-x-4">
-
-                    <div className="font-semibold text-indigo-700">Show Password</div>
+                <div className="flex flex-row items-center space-x-4 text-center mt-11">
+                <div className="items-center justify-center font-bold">Show Password</div>
                     <div className="shadow-md"><input onClick={() => {
                         if (passwordtype === false) {
                             setPasswordtype(true)
@@ -219,11 +205,29 @@ const Login: React.FC<Props> = (props) => {
                             setPasswordtype(false);
                         }
                     }} className="shadow-md cursor-pointer" type="checkbox" /></div>
-                   
-                </div>
+                   <div className="flex flex-row space-x-4">
+                   <div className="ml-32"> <button type="submit" className="p-1 px-4 text-white bg-indigo-500 border-2 rounded-md shadow-md hover:opacity-60">Login</button></div>
 
-                <div className="mt-4"><Link className="pt-8 font-semibold tracking-widest text-blue-800 text-md " to="/auth/forgotpassword">Forgot Password?</Link></div>
+
+                    <div><button type="button" onClick={(event) => {
+                        console.log("cancel button clicked");
+                        event.preventDefault();
+
+                    }} className="p-1 px-4 text-white bg-indigo-500 border-2 rounded-md shadow-md hover:opacity-60">Cancel</button></div>
+                    </div>
+
+                    
+                   
+                   
+
+                </div>
                 
+
+                
+        
+
+                <div className="mt-8 ml-32"><Link className="pt-8 font-semibold tracking-widest text-blue-800 text-md" to="/auth/forgotpassword">Forgot Password?</Link></div>
+                <p className="mt-16 font-semibold text-blue-600">© 2020 All Rights Reserved. Cookie Preferences, Privacy, and Terms.</p>
                
 
             </form>
