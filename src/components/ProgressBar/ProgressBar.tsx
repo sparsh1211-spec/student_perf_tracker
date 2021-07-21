f26f3101d24867883cdc04fad13cff9adc794d01
sparsh1211-spec <84899495+sparsh1211-spec@users.noreply.github.com>
2021-07-21 18:37:29 +0530

    modified progress bar

--- src/components/ProgressBar/ProgressBar.tsx
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -14,15 +14,18 @@ interface Props extends ProgressHTMLAttributes<HTMLProgressElement>{
 const ProgressBar: React.FC<Props>=({progress,className,theme,...rest}) => {
 
     const themeClasses=theme==="primary" ? "bg-indigo-500 ": theme==="danger" ? "bg-red-500 ": theme==="success" ? "bg-green-500 " : theme==="dark" ? "bg-gray-600" : theme==="warning" ? "bg-yellow-500" : "bg-blue-100";
+    
+    const bgClasses=theme==="primary" ? "bg-indigo-100 ": theme==="danger" ? "bg-red-100 ": theme==="success" ? "bg-green-100 " : theme==="dark" ? "bg-gray-100" : theme==="warning" ? "bg-yellow-100" : "bg-blue-100";
+
 
     
     
     return (
        <>
        
-          
+          <div className={"rounded-full " + bgClasses}>
           {progress<=100&&<div className={"h-6 rounded-full "+ className + " " + themeClasses} style={{width: progress + "%" ,}}></div>}
-     
+          </div>
        
        </>
     );
