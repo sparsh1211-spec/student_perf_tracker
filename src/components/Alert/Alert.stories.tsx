3ecad989ff92dce701d8a6341e85e3b0fa32b1fc
sparsh1211-spec <84899495+sparsh1211-spec@users.noreply.github.com>
2021-07-21 15:59:59 +0530

    made reusable alert component and added a stories file for it

---
+++ src/components/Alert/Alert.stories.tsx
@@ -0,0 +1,30 @@
+import Alert from "./Alert";
+import "../../index.css"
+
+
+
+
+export default {
+    title: 'My Alerts',
+    component:Alert ,
+    argTypes: {
+        theme: {
+            control: { type: "select" },
+        },
+       
+        },
+};
+
+
+export const Main = (args: any) => (<Alert {...args}></Alert>);
+
+Main.args = {
+    children: "Sign in",
+    className: "",
+    disabled: false,
+    type: "submit",
+
+   
+}
+
+
