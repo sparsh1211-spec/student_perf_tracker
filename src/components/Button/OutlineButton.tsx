dc9f39559dd75719376f3b7b67caeb964c8d9969
sparsh1211-spec <84899495+sparsh1211-spec@users.noreply.github.com>
2021-07-21 15:16:37 +0530

    modified outline button component

--- src/components/Button/OutlineButton.tsx
+++ src/components/Button/OutlineButton.tsx
@@ -14,7 +14,7 @@ interface Props extends ButtonHTMLAttributes<HTMLButtonElement>{
 
 const OutlineButton: React.FC<Props>=({children,className,theme,Icon,...rest}) => {
 
-    const themeClasses=theme==="primary" ? "border-indigo-500 text-indigo-500 hover:bg-indigo-500 hover:text-white ": theme==="danger" ? "border-red-500 text-red-500 hover:bg-red-500 hover:text-white ": theme==="success" ? "border-green-500 text-green-500 hover:bg-green-500 hover:text-white " : theme==="dark" ? "border-gray-600 text-gray-600 hover:bg-gray-500 hover:text-white " : theme==="warning" ? "border-yellow-500 text-yellow-500 hover:bg-yellow-500 hover:text-white" : "border-blue-100 text-blue-100 hover:bg-blue-500 hover:text-white ";
+    const themeClasses=theme==="primary" ? "border-indigo-500 text-indigo-500 hover:bg-indigo-500 hover:text-white ": theme==="danger" ? "border-red-500 text-red-500 hover:bg-red-500 hover:text-white ": theme==="success" ? "border-green-500 text-green-500 hover:bg-green-500 hover:text-white " : theme==="dark" ? "border-gray-400 text-gray-600 hover:bg-gray-500 hover:text-white " : theme==="warning" ? "border-yellow-500 text-yellow-500 hover:bg-yellow-500 hover:text-white" : "border-blue-100 text-blue-100 hover:bg-blue-500 hover:text-white ";
     const iconThemeClasses=theme==="primary" ? "group-hover:text-indigo-400": "group-hover:text-gray-100";
     return (
         <button {...rest} className={"h-12 w-32 p-2 flex justify-center text-center items-center  border-2 rounded-lg shadow-md " + themeClasses + " " + className}>
